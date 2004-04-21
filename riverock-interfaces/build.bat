fff07d3c6d5bcb0d9793c99f289a714139585945
smissan <smissan@c99c7d4a-b4a9-48f8-bb7e-b54423a2f45b>
2004-04-21 17:27:56 +0000

    Binary to ascii

--- riverock-interfaces/build.bat
+++ riverock-interfaces/build.bat
@@ -1,13 +1,7 @@
 set JAVA_HOME_OLD=%JAVA_HOME%
-
 set JAVA_HOME=c:\jdk1.4.2
 
-
-
 set CLASSPATH=lib\ant-1.5.1-jdk13.jar;%JAVA_HOME%\lib\tools.jar;lib\xml-commons-1.1.1.jar
-
 %JAVA_HOME%\bin\java org.apache.tools.ant.Main -buildfile build-schema.xml %1 %2 %3
-
-
 
 set JAVA_HOME=%JAVA_HOME_OLD%
