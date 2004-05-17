5c373e43bb2a2f21660a48e6c6814d682e6d1c19
serg_main <serg_main@c99c7d4a-b4a9-48f8-bb7e-b54423a2f45b>
2004-05-17 19:17:18 +0000

    .

---
+++ trunk/riverock-sql-parser/run/c.bat
@@ -0,0 +1,5 @@
+set CLASSPATH=%CLASSPATH%;..\..\riverock-lib\lib\antlr-2.7.3.jar
+
+
+
+C:\jdk1.4.2\bin\javac.exe *.java tt/*.java
---
+++ trunk/riverock-sql-parser/run/run.bat
@@ -0,0 +1,5 @@
+set CLASSPATH=%CLASSPATH%;..\..\riverock-lib\lib\antlr-2.7.3.jar;.;tt;
+
+
+
+C:\jdk1.4.2\bin\java.exe Main test.sql
