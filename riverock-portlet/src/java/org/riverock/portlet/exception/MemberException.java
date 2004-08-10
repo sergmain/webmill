1f29f567cfa9dc7aee06f11bf92e5e38bae6e701
Serge Maslyukov <19933474+sergmain@users.noreply.github.com>
2004-08-10 10:53:44 +0000

    init commit

---
+++ riverock-portlet/src/java/org/riverock/portlet/exception/MemberException.java
@@ -0,0 +1,30 @@
+package org.riverock.portlet.exception;
+
+/**
+ * User: smaslyukov
+ * Date: 30.07.2004
+ * Time: 19:56:26
+ * $Id$
+ */
+public class MemberException extends Exception{
+    public MemberException()
+    {
+        super();
+    }
+
+    public MemberException(String s)
+    {
+        super(s);
+    }
+
+    public String toString()
+    {
+        return super.toString();
+    }
+
+    public String getMessage()
+    {
+        return super.getMessage();
+    }
+
+}
