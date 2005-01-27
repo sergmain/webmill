3e5b3722b5ca7503994140927dfbd9ab6daedc75
Serge Maslyukov <19933474+sergmain@users.noreply.github.com>
2005-01-27 08:36:07 +0000

    .

---
+++ riverock-webmill/sql/2004-11-04/ch-008.sql
@@ -0,0 +1,15 @@
+update SITE_CONTENT_CSS a
+set ID_SITE_SUPPORT_LANGUAGE = 
+(select z.ID_SITE_SUPPORT_LANGUAGE 
+from SITE_SUPPORT_LANGUAGE z
+where z.id_site=a.id_site and 
+z.id_language=a.id_language and
+z.CUSTOM_LANGUAGE=a.CUSTOM_LANGUAGE
+)
+/
+
+commit
+/
+
+
+
---
+++ riverock-webmill/sql/2004-11-04/ch-009.sql
@@ -0,0 +1,17 @@
+alter table SITE_CONTENT_CSS
+drop column ID_SITE
+/
+
+alter table SITE_CONTENT_CSS
+drop column ID_LANGUAGE
+/
+
+alter table SITE_CONTENT_CSS
+drop column CUSTOM_LANGUAGE
+/
+
+alter table SITE_CONTENT_CSS
+drop column OLD_ID_SITE_CONTEXT_CSS
+/
+
+
