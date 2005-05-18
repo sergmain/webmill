34274dff75cd5c027b891c0c37a5b5906baf8d9b
serg_main <serg_main@c99c7d4a-b4a9-48f8-bb7e-b54423a2f45b>
2005-05-18 10:52:17 +0000

    .

--- trunk/riverock-webmill/build.bat
+++ trunk/riverock-webmill/build.bat
@@ -1,7 +1,7 @@
 set JAVA_HOME_OLD=%JAVA_HOME%
-set JAVA_HOME=c:\jdk1.4.2
+set JAVA_HOME=c:\j2sdk1.4.2_07
 
-set CLASSPATH=lib\ant-1.5.1-jdk13.jar;%JAVA_HOME%\lib\tools.jar;lib\xml-commons-1.1.1.jar
+set CLASSPATH=..\riverock-lib\lib\ant-1.5.1-jdk13.jar;%JAVA_HOME%\lib\tools.jar;
 %JAVA_HOME%\bin\java -Xms256m -Xmx384m org.apache.tools.ant.Main %1 %2 %3
 
 set JAVA_HOME=%JAVA_HOME_OLD%
