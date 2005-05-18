b75f1cb07378018129069f94145bfe2430fb6d57
Serge Maslyukov <19933474+sergmain@users.noreply.github.com>
2005-05-18 07:49:30 +0000

    .

---
+++ riverock-interfaces/src/java/org/riverock/interfaces/portlet/member/PortletGetList.java
@@ -0,0 +1,13 @@
+package org.riverock.interfaces.portlet.member;
+
+import java.util.List;
+
+/**
+ * @author SMaslyukov
+ *         Date: 19.04.2005
+ *         Time: 14:56:10
+ *         $Id$
+ */
+public interface PortletGetList {
+    public List getList(Long idSiteCtxLangCatalog, Long idContext);
+}    
