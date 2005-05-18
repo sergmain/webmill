f0052127f60fc31a861000033d4736227f404e39
serg_main <serg_main@c99c7d4a-b4a9-48f8-bb7e-b54423a2f45b>
2005-05-18 13:08:42 +0000

    .

---
+++ trunk/build.bat
@@ -0,0 +1,7 @@
+set JAVA_HOME_OLD=%JAVA_HOME%
+set JAVA_HOME=c:\j2sdk1.4.2_07
+
+set CLASSPATH=riverock-lib\lib\ant-1.6.2.jar;riverock-lib\lib\ant-launcher-1.6.2.jar;%JAVA_HOME%\lib\tools.jar;
+%JAVA_HOME%\bin\java -Xms128m -Xmx192m org.apache.tools.ant.Main %1 %2 %3
+
+set JAVA_HOME=%JAVA_HOME_OLD%
