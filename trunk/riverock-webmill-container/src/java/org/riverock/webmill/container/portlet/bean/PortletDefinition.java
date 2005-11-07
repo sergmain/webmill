e63ecc2e5a29198077dc9a24e655e42bb39b76f8
serg_main <serg_main@c99c7d4a-b4a9-48f8-bb7e-b54423a2f45b>
2005-11-07 20:28:43 +0000

    added some import

--- trunk/riverock-webmill-container/src/java/org/riverock/webmill/container/portlet/bean/PortletDefinition.java
+++ trunk/riverock-webmill-container/src/java/org/riverock/webmill/container/portlet/bean/PortletDefinition.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.io.Serializable;
 
+import javax.portlet.PortletPreferences;
+
 /**
  * @author Serge Maslyukov
  *         Created 06.08.2005
