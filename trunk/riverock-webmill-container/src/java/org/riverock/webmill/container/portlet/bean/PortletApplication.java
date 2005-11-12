0f0903723a99a35058b79d24e19a79c125154da2
serg_main <serg_main@c99c7d4a-b4a9-48f8-bb7e-b54423a2f45b>
2005-11-12 16:48:21 +0000

    added copyright

--- trunk/riverock-webmill-container/src/java/org/riverock/webmill/container/portlet/bean/PortletApplication.java
+++ trunk/riverock-webmill-container/src/java/org/riverock/webmill/container/portlet/bean/PortletApplication.java
@@ -1,3 +1,27 @@
+/*
+ * org.riverock.webmill.container -- Webmill portlet container implementation
+ *
+ * Copyright (C) 2004, Riverock Software, All Rights Reserved.
+ *
+ * Riverock -- The Open-source Java Development Community
+ * http://www.riverock.org
+ *
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
 package org.riverock.webmill.container.portlet.bean;
 
 import java.util.ArrayList;
@@ -7,7 +31,6 @@ import java.io.Serializable;
 /**
  * @author Serge Maslyukov
  *         Created 06.08.2005
- *         <p/>
  *         Class PortletAppType.
  * @version $Revision$ $Date$
  */
