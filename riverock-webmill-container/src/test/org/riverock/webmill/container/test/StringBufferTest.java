835b6c7084a2252c148cb9a85a7b8e292b83296d
Serge Maslyukov <19933474+sergmain@users.noreply.github.com>
2005-11-20 14:15:26 +0000

    init commit

---
+++ riverock-webmill-container/src/test/org/riverock/webmill/container/test/StringBufferTest.java
@@ -0,0 +1,16 @@
+package org.riverock.webmill.container.test;
+
+/**
+ * @author SergeMaslyukov
+ *         Date: 20.11.2005
+ *         Time: 1:45:02
+ *         $Id$
+ */
+public class StringBufferTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("buffer: '"  + new StringBuffer().toString() + "'" );
+
+    }
+}
