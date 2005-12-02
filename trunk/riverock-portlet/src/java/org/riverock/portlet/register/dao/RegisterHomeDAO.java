f1a6b0dda33f8e99192cb8c91e5841da046f8491
serg_main <serg_main@c99c7d4a-b4a9-48f8-bb7e-b54423a2f45b>
2005-12-02 17:10:54 +0000

    init commit

---
+++ trunk/riverock-portlet/src/java/org/riverock/portlet/register/dao/RegisterHomeDAO.java
@@ -0,0 +1,10 @@
+package org.riverock.portlet.register.dao;
+
+/**
+ * @author SergeMaslyukov
+ *         Date: 30.11.2005
+ *         Time: 20:55:48
+ *         $Id$
+ */
+public class RegisterHomeDAO {
+}
