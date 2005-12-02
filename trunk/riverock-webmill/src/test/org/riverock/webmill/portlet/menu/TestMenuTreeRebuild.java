503d4cb9c08cf88d3dbc5a282a9a1375c78a14fd
serg_main <serg_main@c99c7d4a-b4a9-48f8-bb7e-b54423a2f45b>
2005-12-02 18:56:47 +0000

    fix package

--- trunk/riverock-webmill/src/test/org/riverock/webmill/portlet/menu/TestMenuTreeRebuild.java
+++ trunk/riverock-webmill/src/test/org/riverock/webmill/portlet/menu/TestMenuTreeRebuild.java
@@ -22,7 +22,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  */
-package org.riverock.portlet.menu;
+package org.riverock.webmill.portlet.menu;
 
 import junit.framework.TestCase;
 import org.riverock.common.collections.TreeUtils;
