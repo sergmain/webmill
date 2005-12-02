7f43892e42cb5f605575a83ea566a5e638ffe980
Serge Maslyukov <19933474+sergmain@users.noreply.github.com>
2005-12-02 17:07:55 +0000

    rename base class XmlResourceBundle to CustomXmlResourceBundle

--- riverock-portlet/src/java/org/riverock/portlet/resource/SiteGame_ru_RU.java
+++ riverock-portlet/src/java/org/riverock/portlet/resource/SiteGame_ru_RU.java
@@ -24,43 +24,17 @@
  */
 package org.riverock.portlet.resource;
 
-import java.util.Iterator;
-
 import org.apache.log4j.Logger;
 
-import org.riverock.webmill.container.resource.XmlResourceBundle;
+import org.riverock.webmill.container.resource.CustomXmlResourceBundle;
 
 /**
  * @author Serge Maslyukov
  */
-public class SiteGame_ru_RU extends XmlResourceBundle {
+public class SiteGame_ru_RU extends CustomXmlResourceBundle {
     private final static Logger log = Logger.getLogger( SiteGame_ru_RU.class );
 
-    private Object[][] resource = null;
-    protected Object[][] getContents() {
-        if (resource!=null) {
-            return resource;
-        }
-
-        PairList list = null;
-        try {
-            list = digestXmlFile();
-        }
-        catch (Exception e) {
-            String es = "Error digest file " +getFileName();
-            log.error(es, e);
-            throw new IllegalStateException(es, e);
-        }
-        resource = new Object[list.getPairs().size()][2];
-        Iterator<Pair> iterator = list.getPairs().iterator();
-        int i=0;
-        while (iterator.hasNext()) {
-            Pair pair = iterator.next();
-            resource[i][0] = pair.getKey();
-            resource[i][1] = pair.getValue();
-            i++;
-        }
-
-        return resource;
+    public void logError( String msg, Throwable th ) {
+        log.error( msg, th );
     }
 }
