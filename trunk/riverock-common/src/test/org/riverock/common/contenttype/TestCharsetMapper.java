14ebc194dc7c6950f3a92aeb2e04d8dd95a6abfc
serg_main <serg_main@c99c7d4a-b4a9-48f8-bb7e-b54423a2f45b>
2006-01-23 17:06:28 +0000

    test of content type service classes

---
+++ trunk/riverock-common/src/test/org/riverock/common/contenttype/TestCharsetMapper.java
@@ -0,0 +1,16 @@
+package org.riverock.common.contenttype;
+
+import junit.framework.TestCase;
+
+/**
+ * @author SergeMaslyukov
+ *         Date: 15.01.2006
+ *         Time: 17:53:45
+ *         $Id$
+ */
+public class TestCharsetMapper extends TestCase {
+
+    public void testCreateCharsetMapper() throws Exception {
+        CharsetMapper charsetMapper = new CharsetMapper();
+    }
+}
