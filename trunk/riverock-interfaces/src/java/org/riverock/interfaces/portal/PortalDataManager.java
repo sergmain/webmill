de36ae7ed2b362452bb7bc8687d329914d77fc2a
serg_main <serg_main@c99c7d4a-b4a9-48f8-bb7e-b54423a2f45b>
2006-01-27 14:18:46 +0000

    init commit

---
+++ trunk/riverock-interfaces/src/java/org/riverock/interfaces/portal/PortalDataManager.java
@@ -0,0 +1,14 @@
+package org.riverock.interfaces.portal;
+
+import java.util.List;
+
+import javax.servlet.http.Cookie;
+
+/**
+ * @author SergeMaslyukov
+ *         Date: 27.01.2006
+ *         Time: 0:44:37
+ *         $Id$
+ */
+public interface PortalDataManager {
+}
---
+++ trunk/riverock-interfaces/src/java/org/riverock/interfaces/sso/a3/AuthInfo.java
@@ -0,0 +1,21 @@
+package org.riverock.interfaces.sso.a3;
+
+/**
+ * @author SergeMaslyukov
+ *         Date: 24.01.2006
+ *         Time: 12:57:00
+ *         $Id$
+ */
+public interface AuthInfo {
+    public Long getAuthUserId();
+    public Long getUserId();
+    public Long getCompanyId();
+    public Long getGroupCompanyId();
+    public Long getHoldingId();
+    public String getUserLogin();
+    public String getUserPassword();
+    public boolean isCompany();
+    public boolean isGroupCompany();
+    public boolean isHolding();
+    public boolean isRoot();
+}
