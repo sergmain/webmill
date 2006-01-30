604239c4907362bb71a460cf6a9e75f6d9f6558c
serg_main <serg_main@c99c7d4a-b4a9-48f8-bb7e-b54423a2f45b>
2006-01-30 22:18:08 +0000

    init commit

---
+++ trunk/riverock-webmill/src/java/org/riverock/webmill/portal/dao/InternalCompanyDao.java
@@ -0,0 +1,22 @@
+package org.riverock.webmill.portal.dao;
+
+import java.util.List;
+
+import org.riverock.interfaces.portal.bean.Company;
+import org.riverock.interfaces.sso.a3.AuthSession;
+
+/**
+ * @author SergeMaslyukov
+ *         Date: 30.01.2006
+ *         Time: 1:24:19
+ *         $Id$
+ */
+public interface InternalCompanyDao {
+    
+    public Company loadCompany( Long id, AuthSession authSession );
+    public List<Company> getCompanyList( AuthSession authSession );
+    public Long processAddCompany( Company companyBean, String userLogin, Long groupCompanyId, Long holdingId, AuthSession authSession );
+    public void processSaveCompany( Company companyBean, AuthSession authSession );
+    public void processDeleteCompany( Company companyBean, AuthSession authSession );
+
+}
