e66f277d3c2443fad238e08819e99609b12d370b
serg_main <serg_main@c99c7d4a-b4a9-48f8-bb7e-b54423a2f45b>
2006-02-13 19:23:03 +0000

    recover of wrong deleted portlets

---
+++ trunk/riverock-portlet/src/java/org/riverock/portlet/login/LoginUtils.java
@@ -0,0 +1,110 @@
+/*
+ * org.riverock.portlet -- Portlet Library
+ *
+ * Copyright (C) 2004, Riverock Software, All Rights Reserved.
+ *
+ * Riverock -- The Open-source Java Development Community
+ * http://www.riverock.org
+ *
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+package org.riverock.portlet.login;
+
+import java.io.IOException;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.apache.log4j.Logger;
+
+import org.riverock.interfaces.portal.PortalSessionManager;
+import org.riverock.portlet.tools.RequestTools;
+import org.riverock.webmill.container.ContainerConstants;
+import org.riverock.common.tools.StringTools;
+
+/**
+ * Author: mill
+ * Date: Dec 2, 2002
+ * Time: 4:42:13 PM
+ * <p/>
+ * $Id$
+ */
+public final class LoginUtils {
+    private final static Logger log = Logger.getLogger( LoginUtils.class );
+
+    static final String CTX_TYPE_LOGIN_PLAIN = "mill.login_plain";
+    static final String CTX_TYPE_LOGIN_XML = "mill.login_xml";
+    public static final String NAME_USERNAME_PARAM = "mill.username";
+    public static final String NAME_PASSWORD_PARAM = "mill.password";
+    public static final String NAME_TOURL_PARAM = "mill.tourl";
+    public static final String LOGIN_CHECK_PORTLET = "mill.login_check";
+
+    public static void check( final ActionRequest actionRequest, final ActionResponse actionResponse )
+        throws IOException {
+
+        String login = actionRequest.getParameter( NAME_USERNAME_PARAM );
+        String password = actionRequest.getParameter( NAME_PASSWORD_PARAM );
+        String url = RequestTools.getString( actionRequest, NAME_TOURL_PARAM );
+        if( url != null ) {
+            url = actionResponse.encodeURL( url );
+        }
+        if( log.isDebugEnabled() ) {
+            log.debug( "userLogin: " + ( login != null ? ( login.toString() + ", class: " + login.getClass().getName() ) : "null" ) );
+            log.debug( "userPassword: " + ( password != null ? ( password.toString() + ", class: " + password.getClass().getName() ) : "null" ) );
+            log.debug( "URL #3: " + url );
+        }
+
+        if ( StringTools.isEmpty( login ) && StringTools.isEmpty( password )) {
+            return;
+        }
+
+        PortalSessionManager sessionManager = (PortalSessionManager)actionRequest.getAttribute( ContainerConstants.PORTAL_PORTAL_SESSION_MANAGER );
+	if (log.isDebugEnabled()) {
+        	ClassLoader cl = Thread.currentThread().getContextClassLoader();
+		log.debug("LoginUtils classLoader: "+cl+"\nhash: "+cl.hashCode() );
+		ClassLoader ccl = sessionManager.getClass().getClassLoader();
+		log.debug("PortalSessionManager impl classLoader: "+ccl+"\nhash: "+ccl.hashCode() );
+	}
+
+
+        boolean checkAccess = sessionManager.createSession( login, password );
+
+        if( checkAccess ) {
+            if( url != null ) {
+                actionResponse.sendRedirect( url );
+            }
+            return;
+        }
+
+        long currentTimeMills = System.currentTimeMillis();
+
+        if( log.isDebugEnabled() )
+            log.debug( "mills " + currentTimeMills );
+
+        try {
+            Thread.sleep( 3000 );
+        }
+        catch( InterruptedException e ) {
+        }
+
+        if( log.isDebugEnabled() )
+            log.debug( "mills " + System.currentTimeMillis() );
+
+        return;
+
+    }
+}
---
+++ trunk/riverock-portlet/src/java/org/riverock/portlet/register/AbstractPortlet.java
@@ -0,0 +1,121 @@
+/*
+ * org.riverock.portlet -- Portlet Library
+ *
+ * Copyright (C) 2004, Riverock Software, All Rights Reserved.
+ *
+ * Riverock -- The Open-source Java Development Community
+ * http://www.riverock.org
+ *
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+package org.riverock.portlet.portlets;
+
+import java.io.IOException;
+import java.util.ResourceBundle;
+
+import javax.portlet.Portlet;
+import javax.portlet.PortletConfig;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.riverock.common.tools.StringTools;
+import org.riverock.module.exception.ActionException;
+import org.riverock.module.exception.ModuleException;
+import org.riverock.module.factory.ActionFactory;
+import org.riverock.module.factory.WebmillPortletActionFactoryImpl;
+import org.riverock.module.web.config.ModuleConfig;
+import org.riverock.module.web.config.PortletModuleConfigImpl;
+import org.riverock.module.web.request.ModuleRequest;
+import org.riverock.module.web.request.WebmillPortletModuleRequestImpl;
+import org.riverock.module.web.response.ModuleResponse;
+import org.riverock.module.web.response.PortletModuleResponseImpl;
+import org.riverock.portlet.main.Constants;
+
+/**
+ * @author SMaslyukov
+ *         Date: 24.03.2005
+ *         Time: 16:03:52
+ *         $Id$
+ */
+public abstract class AbstractPortlet implements Portlet {
+    private final static Log log = LogFactory.getLog(AbstractPortlet.class);
+
+    public static final String ACTION_CONFIG_PARAM_NAME = "action-config";
+
+    protected ActionFactory actionFactory = null;
+    protected ModuleConfig moduleConfig = null;
+
+    public abstract String processSystemError(ModuleRequest request, ResourceBundle resourceBundle);
+
+    public void init(PortletConfig portletConfig) throws PortletException {
+        this.moduleConfig = new PortletModuleConfigImpl(portletConfig);
+        try {
+            actionFactory = new WebmillPortletActionFactoryImpl(moduleConfig, ACTION_CONFIG_PARAM_NAME);
+        }
+        catch (ActionException e) {
+            String es = "Error init action";
+            log.error(es, e);
+            throw new PortletException(es, e);
+        }
+    }
+
+    public void destroy() {
+        if (actionFactory!=null) {
+            actionFactory.destroy();
+        }
+        moduleConfig = null;
+    }
+
+    public void render(RenderRequest renderRequest, RenderResponse renderResponse) throws PortletException, IOException {
+
+        ModuleRequest moduleRequest = new WebmillPortletModuleRequestImpl(renderRequest);
+        ModuleResponse moduleResponse = new PortletModuleResponseImpl(renderResponse);
+        ResourceBundle bundle = moduleConfig.getResourceBundle(renderRequest.getLocale());
+        String forwardPage = (String)renderRequest.getAttribute( Constants.FORWARD_PAGE_ACTION );
+        moduleRequest.setAttribute(Constants.PRINCIPAL_BEAN, moduleRequest.getUser() );
+
+        if (log.isDebugEnabled()) {
+            log.debug("forward attr: " + forwardPage );
+            log.debug("actionMessage attr: " +  renderRequest.getAttribute( Constants.ACTION_MESSAGE ));
+        }
+
+        // forward page
+        Throwable th = null;
+        try {
+            if (!StringTools.isEmpty(forwardPage)) {
+                moduleConfig.getContext().getRequestDispatcher( forwardPage ).include( moduleRequest, moduleResponse );
+            }
+        }
+        catch (Throwable e) {
+            th = e;
+        }
+        if (th!=null) {
+            forwardPage = processSystemError(moduleRequest, bundle );
+            try {
+                moduleConfig.getContext().getRequestDispatcher( forwardPage ).include( moduleRequest, moduleResponse );
+            }
+            catch (ModuleException e) {
+                throw new PortletException("Error include context", e);
+            }
+        }
+    }
+}
