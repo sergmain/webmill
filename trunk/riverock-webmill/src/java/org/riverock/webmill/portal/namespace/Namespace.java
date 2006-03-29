e6fef67b71d83d802617ca2d948179ddedd775e6
serg_main <serg_main@c99c7d4a-b4a9-48f8-bb7e-b54423a2f45b>
2006-03-29 22:54:15 +0000

    init commit

---
+++ trunk/riverock-webmill/src/java/org/riverock/webmill/portal/namespace/Namespace.java
@@ -0,0 +1,11 @@
+package org.riverock.webmill.portal.namespace;
+
+/**
+ * @author SergeMaslyukov
+ *         Date: 08.03.2006
+ *         Time: 2:28:29
+ *         $Id$
+ */
+public interface Namespace {
+    public String getNamespace();
+}
