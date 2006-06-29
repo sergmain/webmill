caee0af2bdc0824a7dc349465cf78fe04f5a136f
serg_main <serg_main@c99c7d4a-b4a9-48f8-bb7e-b54423a2f45b>
2006-06-29 16:38:04 +0000

    fixed copyright

--- trunk/riverock-webmill/src/java/org/riverock/webmill/portal/ContextNavigator.java
+++ trunk/riverock-webmill/src/java/org/riverock/webmill/portal/ContextNavigator.java
@@ -1,13 +1,13 @@
 /*
  * org.riverock.webmill -- Portal framework implementation
  *
- * Copyright (C) 2004, Riverock Software, All Rights Reserved.
+ * Copyright (C) 2006, Riverock Software, All Rights Reserved.
  *
  * Riverock -- The Open-source Java Development Community
  * http://www.riverock.org
  *
  *
- * This program is free software; you can redistribute it and/or
+ * This program is free software; you can redistribute it and/or             
  * modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation; either
  * version 2 of the License, or (at your option) any later version.
@@ -20,7 +20,6 @@
  * You should have received a copy of the GNU General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
  */
 package org.riverock.webmill.portal;
 
