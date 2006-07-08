f94fc50ebff2a9056942a2fdc3319352422a461f
Serge Maslyukov <19933474+sergmain@users.noreply.github.com>
2006-07-08 17:10:11 +0000

    fix SF-1519281 (Implement family of DB for remove dependence in config files)

--- riverock-generic/src/java/org/riverock/generic/db/DbConnectionProvider.java
+++ riverock-generic/src/java/org/riverock/generic/db/DbConnectionProvider.java
@@ -3,12 +3,20 @@ package org.riverock.generic.db;
 import java.util.Map;
 import java.util.HashMap;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
 import org.apache.log4j.Logger;
 
 import org.riverock.generic.schema.config.DatabaseConnectionType;
 import org.riverock.generic.exception.DatabaseException;
 import org.riverock.generic.db.factory.ORAconnect;
 import org.riverock.generic.db.factory.MYSQLconnect;
+import org.riverock.generic.db.factory.HSQLconnect;
+import org.riverock.generic.db.factory.MSSQL_JTDS_connect;
+import org.riverock.generic.db.factory.PostgreeSQLconnect;
+import org.riverock.generic.db.factory.IBMDB2connect;
+import org.riverock.generic.db.factory.SAPconnect;
 import org.riverock.common.tools.MainTools;
 
 /**
@@ -19,11 +27,19 @@ import org.riverock.common.tools.MainTools;
 public class DbConnectionProvider {
     private final static Logger log = Logger.getLogger(DbConnectionProvider.class);
 
-    private static Map<String, String> famalyMap = new HashMap<String, String>();
+    private static Map<String, String> familyMap = new HashMap<String, String>();
     static {
-        famalyMap.put("oracle", ORAconnect.class.getName());
-        famalyMap.put("mysql", MYSQLconnect.class.getName());
+        familyMap.put("oracle", ORAconnect.class.getName());
+        familyMap.put("mysql", MYSQLconnect.class.getName());
+        familyMap.put("hsql", HSQLconnect.class.getName());
+        familyMap.put("mssql", MSSQL_JTDS_connect.class.getName());
+        familyMap.put("postgrees", PostgreeSQLconnect.class.getName());
+        familyMap.put("db2", IBMDB2connect.class.getName());
+        familyMap.put("sap", SAPconnect.class.getName());
     }
+
+    private static final Object syncObject=new Object();
+    private static Map<String, String> familyClassMap = new HashMap<String, String>();
 
     static DatabaseAdapter openConnect(final DatabaseConnectionType dc)
         throws DatabaseException {
@@ -33,17 +49,50 @@ public class DbConnectionProvider {
             throw new DatabaseException(es);
         }
 
+        String className=null;
+        if (dc.getFamily()!=null) {
+            className = familyClassMap.get(dc.getFamily());
+            if (className==null) {
+                synchronized(syncObject) {
+                    className = familyClassMap.get(dc.getFamily());
+                    if (className==null) {
+                        String name = "java:comp/env/"+dc.getFamily() ;
+                        try {
+                            InitialContext ic = new InitialContext();
+                            String familyName = (String) ic.lookup( name );
+                            className = familyMap.get(familyName);
+                            if (className!=null) {
+                                familyClassMap.put(dc.getFamily(), className);
+                            }
+                        }
+                        catch (NamingException e) {
+                            String es = "Error get value of DB family from JNDI. JNDI env: " +dc.getFamily();
+                            log.error(es, e);
+                            throw new IllegalArgumentException(es, e);
+                        }
+                    }
+                }
+            }
+        }
+        else if (dc.getConnectionClass()!=null) {
+            className = dc.getConnectionClass();
+        }
+
+        if (className==null) {
+            throw new IllegalArgumentException("Error configure db connection. Neither 'family' and 'className' are defined");
+        }
+
         DatabaseAdapter db_ = null;
         try {
             if (log.isDebugEnabled()) {
-                log.debug("Call for create dynamic object " + dc.getConnectionClass());
+                log.debug("Call for create dynamic object " + className);
             }
 
-            db_ = (DatabaseAdapter) MainTools.createCustomObject(dc.getConnectionClass());
+            db_ = (DatabaseAdapter) MainTools.createCustomObject(className);
             db_.init(dc);
 
             if (log.isDebugEnabled()) {
-                log.debug("Success create dynamic object " + dc.getConnectionClass());
+                log.debug("Success create dynamic object " + className);
             }
         }
         catch (Exception e) {
@@ -58,10 +107,11 @@ public class DbConnectionProvider {
             }
 
             log.fatal("Error create instance for class " + dc.getConnectionClass());
+            log.fatal("Error create instance for family " + dc.getFamily());
             log.fatal("ConnectionName - " + dc.getName());
             log.fatal("Error:", e);
 
-            final String es = "Error create instance for class " + dc.getConnectionClass() + ". See log for details";
+            final String es = "Error create DB instance. See log for details";
             System.out.println(es);
             throw new DatabaseException(es, e);
         }
