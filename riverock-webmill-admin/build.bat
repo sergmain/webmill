9f8d471fc44263c34be7bbedfea1b855630ee004
Serge Maslyukov <19933474+sergmain@users.noreply.github.com>
2006-07-14 16:33:13 +0000

    .

--- riverock-webmill-admin/build.bat
+++
@@ -1,3 +0,0 @@
-set CLASSPATH=..\riverock-lib\lib\ant-1.6.2.jar;..\riverock-lib\lib\ant-launcher-1.6.2.jar;%JAVA_HOME%\lib\tools.jar;
-
-%JAVA_HOME%\bin\java -Xms64m -Xmx128m org.apache.tools.ant.Main %*
