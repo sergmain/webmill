02fc194fceb8508d59845506e575d52657da4575
Serge Maslyukov <19933474+sergmain@users.noreply.github.com>
2006-07-18 14:32:33 +0000

    update of copyright

--- riverock-generic/src/main/java/org/riverock/generic/startup/StartupException.java
+++ riverock-generic/src/main/java/org/riverock/generic/startup/StartupException.java
@@ -1,12 +1,14 @@
 /*
- * org.riverock.generic -- Database connectivity classes
- * 
- * Copyright (C) 2004, Riverock Software, All Rights Reserved.
- * 
- * Riverock -- The Open-source Java Development Community
+ * org.riverock.generic - Database connectivity classes, part of Webmill portal
+ * For more information about Webmill portal, please visit project site
+ * http://webmill.askmore.info
+ *
+ * Copyright (C) 2000-2006, Riverock Software, All Rights Reserved.
+ *
+ * Riverock - The Open-source Java Development Community
  * http://www.riverock.org
- * 
- * 
+ *
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
@@ -20,7 +22,6 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
  */
 package org.riverock.generic.startup;
 
