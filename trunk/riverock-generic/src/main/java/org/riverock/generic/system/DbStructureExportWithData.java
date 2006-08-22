602158ba26cff6d8effe1ef1acf1a68a45887194
serg_main <serg_main@c99c7d4a-b4a9-48f8-bb7e-b54423a2f45b>
2006-08-22 21:12:31 +0000

    Fixed list of imports

--- trunk/riverock-generic/src/main/java/org/riverock/generic/system/DbStructureExportWithData.java
+++ trunk/riverock-generic/src/main/java/org/riverock/generic/system/DbStructureExportWithData.java
@@ -25,21 +25,7 @@
  */
 package org.riverock.generic.system;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.util.ArrayList;
-
-import org.riverock.common.config.PropertiesProvider;
-import org.riverock.generic.db.DatabaseAdapter;
-import org.riverock.generic.db.DatabaseManager;
-import org.riverock.generic.schema.db.structure.DbSchemaType;
-import org.riverock.generic.schema.db.structure.DbTableType;
-import org.riverock.generic.schema.db.structure.DbViewType;
-import org.riverock.generic.tools.XmlTools;
 import org.riverock.generic.startup.StartupApplication;
-
-import org.exolab.castor.xml.Unmarshaller;
-import org.xml.sax.InputSource;
 
 /**
  * Export data from DB to XML file
