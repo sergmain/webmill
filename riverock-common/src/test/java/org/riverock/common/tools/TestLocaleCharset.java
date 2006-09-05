32f5c1b02fe8723eabdd577c6e23bb70771aaf8f
Serge Maslyukov <19933474+sergmain@users.noreply.github.com>
2006-09-05 17:12:18 +0000

    Write some test cases for improve of coverage

---
+++ riverock-common/src/test/java/org/riverock/common/tools/TestLocaleCharset.java
@@ -0,0 +1,53 @@
+/*
+ * org.riverock.common - Supporting classes, interfaces, and utilities
+ *
+ * Copyright (C) 2006, Riverock Software, All Rights Reserved.
+ *
+ * Riverock - The Open-source Java Development Community
+ * http://www.riverock.org
+ *
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+package org.riverock.common.tools;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import junit.framework.TestCase;
+
+/**
+ * LocaleCharset Tester.
+ *
+ * @author <Authors name>
+ * @since <pre>09/05/2006</pre>
+ * @version 1.0
+ * 
+ * $Id$
+ */
+public class TestLocaleCharset extends TestCase {
+
+    public void testConstructor() throws Exception {
+       new LocaleCharset();
+    }
+
+    public void testGetCharset() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    public static Test suite() {
+        return new TestSuite(TestLocaleCharset.class);
+    }
+}
