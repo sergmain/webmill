99d2591721f33d86df3e222f23bdd9eece07daae
serg_main <serg_main@c99c7d4a-b4a9-48f8-bb7e-b54423a2f45b>
2006-09-05 17:12:18 +0000

    Write some test cases for improve of coverage

---
+++ trunk/riverock-common/src/test/java/org/riverock/common/html/TestHeader.java
@@ -0,0 +1,89 @@
+/*
+ * org.riverock.common - Supporting classes, interfaces, and utilities
+ *
+ * Copyright (C) 2006, Riverock Software, All Rights Reserved.
+ *
+ * Riverock - The Open-source Java Development Community
+ * http://www.riverock.org
+ *
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+package org.riverock.common.html;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import junit.framework.TestCase;
+
+/**
+ * Header Tester.
+ *
+ * @author <Authors name>
+ * @since <pre>09/05/2006</pre>
+ * @version 1.0
+ * 
+ * $Id$
+ */
+public class TestHeader extends TestCase {
+
+    public void testConstructor() throws Exception {
+       new Header();
+    }
+
+    public void testGetUserAgent() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    public void testGetAcceptLanguageArray() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    public void testGetAcceptLanguageArray1() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    public void testGetAcceptLanguageAsList() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    public void testGetAcceptLanguageAsLocaleListSorted() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    public void testGetAcceptLanguageAsLocaleListSorted1() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    public void testGetAcceptLanguageAsList1() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    public void testGetAcceptLanguage() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    public void testGetReferer() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    public void testGetTypeBrowser() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    public static Test suite() {
+        return new TestSuite(TestHeader.class);
+    }
+}
