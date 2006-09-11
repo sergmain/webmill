d026efecd1e7f7e3fb610c05ad78758660fc2df2
serg_main <serg_main@c99c7d4a-b4a9-48f8-bb7e-b54423a2f45b>
2006-09-11 18:44:01 +0000

    Work on 'Webclip' portlet

---
+++ trunk/riverock-portlet/src/main/java/org/riverock/portlet/webclip/WebclipUrlProducer.java
@@ -0,0 +1,58 @@
+/*
+ * org.riverock.portlet - Portlet Library
+ *
+ * Copyright (C) 2006, Riverock Software, All Rights Reserved.
+ *
+ * Riverock - The Open-source Java Development Community
+ * http://www.riverock.org
+ *
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package org.riverock.portlet.webclip;
+
+/**
+ * @author Sergei Maslyukov
+ *         Date: 11.09.2006
+ *         Time: 22:07:04
+ *         <p/>
+ *         $Id$
+ */
+public interface WebclipUrlProducer {
+    
+    /**
+     * установить текущий хреф
+     * @param value текущий хреф
+     */
+    void setCurrentHrefValue(String value);
+
+    /**
+     * установить параметр урла
+     * @param name
+     * @param value
+     */
+    void setParameter(String name, String value);
+
+    /**
+     * получить урла
+     * @return String
+     */
+    String getUrl();
+
+    /**
+     * инитим создателя
+     */
+    void init();
+}
