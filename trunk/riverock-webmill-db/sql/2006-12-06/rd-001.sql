38d1d5fa6938872deee3a1935a1f9cc0dbc880b5
serg_main <serg_main@c99c7d4a-b4a9-48f8-bb7e-b54423a2f45b>
2006-12-06 21:34:19 +0000

    Work on SF-1610366 (Switch commerce portlet from jdbc to hibernate)

---
+++ trunk/riverock-webmill-db/sql/2006-12-06/rd-001.sql
@@ -0,0 +1,21 @@
+insert into wm_portal_ids
+(select 'wm_price_shop_list' name, max(ID_SHOP)+1 max_value from wm_price_shop_list)
+/
+
+insert into wm_portal_ids
+(select 'wm_cash_currency_std' name, max(ID_STD_CURR)+1 max_value from wm_cash_currency_std)
+/
+
+insert into wm_portal_ids
+(select 'wm_price_shop_precision' name, max(ID_PRICE_SHOP_PRECISION)+1 max_value from wm_price_shop_precision)
+/
+
+insert into wm_portal_ids
+(select 'wm_cash_curr_value' name, max(ID_CURVAL)+1 max_value from wm_cash_curr_value)
+/
+
+insert into wm_portal_ids
+(select 'wm_cash_curs_std' name, max(ID_STD_CURS)+1 max_value from wm_cash_curs_std)
+/
+
+
