563489ffac2c56884ae9ca183ffd9af556463fe7
serg_main <serg_main@c99c7d4a-b4a9-48f8-bb7e-b54423a2f45b>
2007-02-22 19:41:42 +0000

    Work on SF-1666341 (Add interface for export current DB structure and data)

--- trunk/riverock-webmill-admin/src/main/java/org/riverock/webmill/admin/servlet/DbExportServlet.java
+++ trunk/riverock-webmill-admin/src/main/java/org/riverock/webmill/admin/servlet/DbExportServlet.java
@@ -1,29 +1,27 @@
 package org.riverock.webmill.admin.servlet;
 
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.riverock.dbrevision.annotation.schema.db.DbSchema;
+import org.riverock.dbrevision.annotation.schema.db.DbTable;
 import org.riverock.dbrevision.db.DatabaseAdapter;
 import org.riverock.dbrevision.db.DatabaseManager;
 import org.riverock.dbrevision.db.DatabaseStructureManager;
 import org.riverock.dbrevision.db.DbConnectionProvider;
-import org.riverock.dbrevision.db.factory.ORAconnect;
-import org.riverock.dbrevision.offline.DbRevisionConfig;
-import org.riverock.dbrevision.annotation.schema.db.DbSchema;
-import org.riverock.dbrevision.annotation.schema.db.DbTable;
-import org.riverock.dbrevision.utils.Utils;
 import org.riverock.dbrevision.exception.DbRevisionException;
-import org.riverock.dbrevision.system.DbStructureImport;
+import org.riverock.dbrevision.utils.Utils;
 import org.riverock.webmill.utils.HibernateUtils;
-import org.apache.log4j.Logger;
-import org.hibernate.Session;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import java.io.OutputStream;
-import java.io.IOException;
-import java.io.FileOutputStream;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.ServletException;
+import java.io.*;
 import java.sql.Connection;
+import java.util.zip.GZIPOutputStream;
 
 /**
  * User: SMaslyukov
@@ -32,11 +30,14 @@ import java.sql.Connection;
  */
 public class DbExportServlet extends HttpServlet {
     private static Logger log = Logger.getLogger(DbExportServlet.class);
+    private static final int BUFFER_SIZE = 2048;
+    private static final String APPLICATION_X_GZIP_CONTENT_TYPE = "application/x-gzip";
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/xml");
-        OutputStream os = response.getOutputStream();
-        
         String family;
         String name = "java:comp/env/dbFamily";
         try {
@@ -55,13 +56,73 @@ public class DbExportServlet extends HttpServlet {
         Connection connection = session.connection();
         DatabaseAdapter db  = DbConnectionProvider.openConnect(connection, family);
 
-        export(db, os, true);
-
+        File tempDir = (File) getServletConfig().getServletContext().getAttribute("javax.servlet.context.tempdir");
+        File tempFile = File.createTempFile("db-struct", ".xml", tempDir);
+        System.out.println("Output xml data to tempFile: " + tempFile);
+        FileOutputStream fos = new FileOutputStream(tempFile);
+        export(db, fos, true);
         session.getTransaction().commit();
+
+        fos.flush();
+        fos.close();
+        fos = null;
+
+        System.out.println("Finish marshaling XML data");
+
+        File gzipFile = outputToGzip(tempFile);
+        System.out.println("Finish output to .gz file");
+
+        FileInputStream fis = new FileInputStream(gzipFile);
+        response.setContentType(APPLICATION_X_GZIP_CONTENT_TYPE);
+        OutputStream os = response.getOutputStream();
+
+        int count = 0;
+        byte[] bytes = new byte[BUFFER_SIZE];
+        while( (count=fis.read())!=-1 ) {
+            os.write(bytes, 0, count);
+        }
+        fis.close();
+        fis = null;
 
         os.flush();
         os.close();
-        os = null;
+        os=null;
+
+
+        System.out.println(
+                "Output xml data to\n" +
+                        "    tempFile: " + tempFile + "\n" +
+                        "    gzipFile: " + gzipFile + "\n" +
+                "\nFinish output data to http response."
+        );
+    }
+
+    private static File outputToGzip(File tempFile) throws IOException {
+
+        File gzipFile = File.createTempFile("db-struct", ".gz", tempFile.getParentFile());
+
+        FileInputStream fis = new FileInputStream(tempFile);
+
+        FileOutputStream fileOutputStream = new FileOutputStream(gzipFile);
+        GZIPOutputStream gzipOutputStream = new GZIPOutputStream(fileOutputStream);
+
+        int count = 0;
+        byte[] bytes = new byte[BUFFER_SIZE];
+        while( (count=fis.read())!=-1 ) {
+            gzipOutputStream.write(bytes, 0, count);
+        }
+        fis.close();
+        fis = null;
+
+        gzipOutputStream.flush();
+        gzipOutputStream.close();
+        gzipOutputStream=null;
+        
+        fileOutputStream.flush();
+        fileOutputStream.close();
+        fileOutputStream=null;
+
+        return gzipFile;
     }
 
     public static void export(DatabaseAdapter db, OutputStream outputStream, boolean isData) {
