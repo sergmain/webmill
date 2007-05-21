8ab1fdd0e802a6b6909e25ccc5599decf03602b7
serg_main <serg_main@c99c7d4a-b4a9-48f8-bb7e-b54423a2f45b>
2007-05-21 19:16:12 +0000

    Added check 'URL not exist' to webclip process action

---
+++ trunk/riverock-portlet/src/test/java/org/riverock/portlet/test/webclip/ParseParameterTest.java
@@ -0,0 +1,29 @@
+package org.riverock.portlet.test.webclip;
+
+import java.util.List;
+
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.httpclient.URIException;
+import org.apache.commons.httpclient.util.ParameterParser;
+import org.apache.commons.httpclient.util.URIUtil;
+
+import org.riverock.portlet.webclip.WebclipDataProcessorImpl;
+
+/**
+ * User: SMaslyukov
+ * Date: 21.05.2007
+ * Time: 22:35:45
+ */
+public class ParseParameterTest {
+    private final static ParameterParser PARAMETER_PARSER = new ParameterParser();
+
+    public static void main(String[] args) throws URIException {
+//        String s = "/w/index.php?title=Template_talk:Niger-Congo-speaking_nations&action=edit";
+        String s = "http://www.dodge.com/sprinter/index.html?context=vehiclePage&type=vehicleLink";
+        s = URIUtil.decode(s);
+        List<NameValuePair> params = PARAMETER_PARSER.parse(s, WebclipDataProcessorImpl.SEPARATOR_HTTP_REQUEST_PARAM);
+        System.out.println("params = " + params);
+        params = PARAMETER_PARSER.parse(s.substring(s.indexOf(WebclipDataProcessorImpl.QUERY_SEPARATOR_HTTP_REQUEST_PARAM)+1), WebclipDataProcessorImpl.SEPARATOR_HTTP_REQUEST_PARAM);
+        System.out.println("params = " + params);
+    }
+}
