75f7bdc5fdcaab3cfeaeb7af728209dd6518240b
serg_main <serg_main@c99c7d4a-b4a9-48f8-bb7e-b54423a2f45b>
2007-07-02 15:27:45 +0000

    Removed duplicated class

--- trunk/riverock-portlet/src/main/java/org/riverock/portlet/article/ArticleXmlPortlet.java
+++ trunk/riverock-portlet/src/main/java/org/riverock/portlet/article/ArticleXmlPortlet.java
@@ -31,7 +31,7 @@ import javax.portlet.PortletException;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
-import org.riverock.portlet.GenericWebmillPortlet;
+import org.riverock.common.portlet.GenericWebmillPortlet;
 
 /**
  * Author: SergeMaslyukov
