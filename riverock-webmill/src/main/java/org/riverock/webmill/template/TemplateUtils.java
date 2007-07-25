07917d368ec3a9f45583500bc1b00861b886df52
Serge Maslyukov <19933474+sergmain@users.noreply.github.com>
2007-07-25 16:58:00 +0000

    New format of template

---
+++ riverock-webmill/src/main/java/org/riverock/webmill/template/TemplateUtils.java
@@ -0,0 +1,108 @@
+package org.riverock.webmill.template;
+
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.beanutils.PropertyUtils;
+
+import org.riverock.webmill.template.schema.Html;
+import org.riverock.webmill.template.schema.Portlet;
+import org.riverock.webmill.template.schema.Custom;
+import org.riverock.webmill.template.schema.Dynamic;
+import org.riverock.webmill.template.schema.Template;
+import org.riverock.webmill.template.schema.SiteTemplate;
+import org.riverock.webmill.template.schema.SiteTemplateItem;
+import org.riverock.webmill.template.schema.Parameter;
+import org.riverock.webmill.template.schema.ElementParameter;
+import org.riverock.webmill.portal.template.bean.types.PortalTemplateItemTypeImpl;
+import org.riverock.interfaces.portal.template.PortalTemplateItemType;
+
+/**
+ * User: SMaslyukov
+ * Date: 25.07.2007
+ * Time: 14:33:39
+ */
+public class TemplateUtils {
+
+    public static List<Object> getElements(Html html) {
+        List<Object> elements = new ArrayList<Object>();
+        try {
+            getElements(html, elements);
+        }
+        catch (Exception e) {
+            throw new RuntimeException("Error process Html object", e);
+        }
+        return elements;
+    }
+
+    private static void getElements(Object obj, List<Object> elements) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        if (obj==null) {
+            return;
+        }
+        if  ((obj instanceof Portlet) || (obj instanceof Custom) || (obj instanceof Dynamic)) {
+            elements.add(obj);
+            return;
+        }
+
+        PropertyDescriptor[] propertyDescriptors = PropertyUtils.getPropertyDescriptors(obj);
+        for (PropertyDescriptor propertyDescriptor : propertyDescriptors) {
+            Object o = PropertyUtils.getProperty(obj, propertyDescriptor.getName());
+            if (o==null) {
+                continue;
+            }
+            if (o instanceof List) {
+                for (Object o1 : (List)o) {
+                    getElements(o1, elements);
+                }
+                continue;
+            }
+            if (!(o.getClass().getPackage().getName().equals(Html.class.getPackage().getName()))) {
+                continue;
+            }
+            if  ((o instanceof Portlet) || (o instanceof Custom) || (o instanceof Dynamic)) {
+                elements.add(o);
+            }
+            else {
+                getElements(o, elements);
+            }
+        }
+    }
+
+    public static Template convertTemplate(SiteTemplate siteTemplate) {
+        Template t = new Template();
+        if (siteTemplate==null) {
+            return t;
+        }
+
+        for (SiteTemplateItem templateItem : siteTemplate.getSiteTemplateItem()) {
+            switch (PortalTemplateItemTypeImpl.valueOf(templateItem.getType()).getType()) {
+                case PortalTemplateItemType.CUSTOM_TYPE:
+                    Custom custom = new Custom();
+                    custom.setValue( templateItem.getValue() );
+                    t.getPortletOrDynamicOrCustom().add(custom);
+                    break;
+
+                case PortalTemplateItemType.DYNAMIC_TYPE:
+                    t.getPortletOrDynamicOrCustom().add(new Dynamic());
+                    break;
+
+                case PortalTemplateItemType.PORTLET_TYPE:
+                    Portlet portlet = new Portlet();
+                    portlet.setCode(templateItem.getCode());
+                    portlet.setValue(templateItem.getValue());
+                    portlet.setXmlRoot(templateItem.getXmlRoot());
+                    for (Parameter parameter : templateItem.getParameter()) {
+                        ElementParameter p = new ElementParameter();
+                        p.setName(parameter.getName());
+                        p.setValue(parameter.getValue());
+                        portlet.getElementParameter().add(p);
+                    }
+                    t.getPortletOrDynamicOrCustom().add(portlet);
+                    break;
+            }
+        }
+        return t;
+    }
+}
---
+++ riverock-webmill/src/test/java/org/riverock/webmill/template/TestParsingTemplateV2.java
@@ -0,0 +1,212 @@
+package org.riverock.webmill.template;
+
+import java.io.InputStream;
+import java.util.List;
+import java.lang.Object;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+
+import junit.framework.TestCase;
+
+import org.riverock.common.tools.XmlTools;
+import org.riverock.webmill.template.schema.*;
+
+/**
+ * User: SMaslyukov
+ * Date: 24.07.2007
+ * Time: 19:38:35
+ */
+public class TestParsingTemplateV2 extends TestCase {
+
+    public void testNullOperation() {
+        List<Object> elements = TemplateUtils.getElements(null);
+        assertNotNull(elements);
+        assertTrue(elements.isEmpty());
+    }
+
+    public void testConvertNullToTemplate() throws Exception {
+        Template t = TemplateUtils.convertTemplate(null);
+        assertNotNull(t);
+    }
+
+    public void testConvertTemplate() throws Exception {
+
+        InputStream is;
+        SiteTemplate siteTemplate;
+
+        is = TestParsingTemplateV2.class.getResourceAsStream("/xml/resources/template/template_v2_1.xml");
+        siteTemplate = XmlTools.getObjectFromXml(SiteTemplate.class, is);
+        assertNotNull(siteTemplate);
+
+        Template t = TemplateUtils.convertTemplate(siteTemplate);
+        assertNotNull(t);
+
+        assertEquals(siteTemplate.getSiteTemplateItem().size(), t.getPortletOrDynamicOrCustom().size());
+
+/*
+        <SiteTemplateItem type="custom" value="HeaderStart"/>
+        <SiteTemplateItem type="custom" value="HeaderText"/>
+        <SiteTemplateItem type="custom" value="HeaderEnd"/>
+        <SiteTemplateItem type="portlet" value="mill.menu" code="TOP_MENU" xmlRoot="TopMenu" role="webmill.authentic"/>
+        <SiteTemplateItem type="portlet" value="mill.login_xml" xmlRoot="LoginOnIndexPage"/>
+        <SiteTemplateItem type="custom" value="MainStart"/>
+        <SiteTemplateItem type="portlet" value="mill.menu"/>
+        <SiteTemplateItem type="portlet" value="mill.menu_member"/>
+        <SiteTemplateItem type="custom" value="Separator"/>
+        <SiteTemplateItem type="portlet" value="mill.news_block"/>     <-- 9
+        <SiteTemplateItem type="portlet" value="mill.menu" code="DEFAULT_ru_RU">
+            <Parameter name="level" value="1"/>
+            <Parameter name="type_level" value="equal"/>
+        </SiteTemplateItem>
+        <SiteTemplateItem type="portlet" value="mill.menu" code="DEFAULT_ru_RU">
+            <Parameter name="level" value="1"/>
+            <Parameter name="type_level" value="great"/>
+        </SiteTemplateItem>
+        <SiteTemplateItem type="custom" value="Footer"/>
+        <SiteTemplateItem type="dynamic"/>
+*/
+        assertEquals("custom", siteTemplate.getSiteTemplateItem().get(0).getType());
+        assertEquals("portlet", siteTemplate.getSiteTemplateItem().get(3).getType());
+        assertEquals("portlet", siteTemplate.getSiteTemplateItem().get(9).getType());
+        assertEquals("dynamic", siteTemplate.getSiteTemplateItem().get(13).getType());
+        SiteTemplateItem item = siteTemplate.getSiteTemplateItem().get(11);
+        assertEquals("portlet", item.getType());
+        assertEquals("mill.menu", item.getValue());
+        assertEquals("DEFAULT_ru_RU", item.getCode());
+        assertEquals(2, item.getParameter().size());
+        Parameter p;
+        p = item.getParameter().get(0);
+        assertEquals("level", p.getName());
+        assertEquals("1", p.getValue());
+
+        p = item.getParameter().get(1);
+        assertEquals("type_level", p.getName());
+        assertEquals("great", p.getValue());
+
+
+        assertEquals(Custom.class, t.getPortletOrDynamicOrCustom().get(0).getClass());
+        assertEquals(Portlet.class, t.getPortletOrDynamicOrCustom().get(3).getClass());
+        assertEquals(Portlet.class, t.getPortletOrDynamicOrCustom().get(9).getClass());
+        assertEquals(Dynamic.class, t.getPortletOrDynamicOrCustom().get(13).getClass());
+        Object o = t.getPortletOrDynamicOrCustom().get(11);
+        assertEquals(Portlet.class, o.getClass());
+        Portlet portlet = (Portlet)o;
+
+        assertEquals("mill.menu", portlet.getValue());
+        assertEquals("DEFAULT_ru_RU", portlet.getCode());
+        assertEquals(2, portlet.getElementParameter().size());
+
+        ElementParameter ep;
+        ep = portlet.getElementParameter().get(0);
+        assertEquals("level", ep.getName());
+        assertEquals("1", ep.getValue());
+
+        ep = portlet.getElementParameter().get(1);
+        assertEquals("type_level", ep.getName());
+        assertEquals("great", ep.getValue());
+    }
+
+
+    public void testUnmarshalTemplate_v2_1() throws Exception {
+        InputStream is;
+        SiteTemplate siteTemplate;
+
+        is = TestParsingTemplateV2.class.getResourceAsStream("/xml/resources/template/template_v2_1.xml");
+        siteTemplate = XmlTools.getObjectFromXml(SiteTemplate.class, is);
+        assertNotNull(siteTemplate);
+    }
+
+    public void testUnmarshalTemplate_v2_2() throws Exception {
+        InputStream is;
+        Template siteTemplate;
+
+        is = TestParsingTemplateV2.class.getResourceAsStream("/xml/resources/template/template_v2_2.xml");
+
+        Source source =  new StreamSource(is);
+        JAXBContext jaxbContext = JAXBContext.newInstance (Template.class.getPackage().getName());
+        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+        JAXBElement<Template> jaxbElement;
+        jaxbElement = unmarshaller.unmarshal( source, Template.class);
+
+        siteTemplate = jaxbElement.getValue();
+        assertNotNull(siteTemplate);
+    }
+
+    public void testUnmarshalTemplate_v2_2_2() throws Exception {
+
+        InputStream is;
+        Template template;
+        is = TestParsingTemplateV2.class.getResourceAsStream("/xml/resources/template/template_v2_2.xml");
+        template = XmlTools.getObjectFromXml(Template.class, is);
+        assertNotNull(template);
+    }
+
+    public void testUnmarshalTemplate_v2_3() throws Exception {
+        InputStream is = TestParsingTemplateV2.class.getResourceAsStream("/xml/resources/template/template_v2_3.xml");
+        Template template = XmlTools.getObjectFromXml(Template.class, is);
+        assertNotNull(template);
+    }
+
+    public void testUnmarshalTemplate_v2_4() throws Exception {
+
+        InputStream is;
+        is = TestParsingTemplateV2.class.getResourceAsStream("/xml/resources/template/template_v2_4.xml");
+        Template template = XmlTools.getObjectFromXml(Template.class, is);
+        assertNotNull(template);
+
+        Html html = template.getHtml();
+        assertNotNull(html);
+
+/*
+        <?xml version="1.0" encoding="UTF-8"?>
+        <Template xmlns:element="http://webmill.riverock.org/xsd/riverock-template-page-elements.xsd"
+            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xsi:noNamespaceSchemaLocation="http://webmill.riverock.org/xsd/riverock-template.xsd">
+        <html>
+            <head>
+                <title>aaaa</title>
+            </head>
+            <body>
+                <element:custom value="Aaa"/>
+                <element:dynamic/>
+                <element:portlet value="webmill:search" xmlRoot="SearchXml" code="SEARCH_CODE"/>
+            </body>
+        </html>
+        </Template>
+*/
+
+        assertNotNull(html.getHead());
+        assertNotNull(html.getHead().getContent());
+        assertEquals(1, html.getHead().getContent().size());
+        assertTrue(html.getHead().getContent().get(0) instanceof Title);
+
+        assertEquals("aaaa", ((Title)html.getHead().getContent().get(0)).getContent());
+
+        assertNotNull(html.getBody());
+        assertNotNull(html.getBody().getPOrH1OrH2());
+        assertEquals(3, html.getBody().getPOrH1OrH2().size());
+
+        assertTrue(html.getBody().getPOrH1OrH2().get(0) instanceof Custom);
+        assertEquals("Aaa", ((Custom)html.getBody().getPOrH1OrH2().get(0)).getValue());
+
+        assertTrue(html.getBody().getPOrH1OrH2().get(1) instanceof Dynamic);
+
+        assertTrue(html.getBody().getPOrH1OrH2().get(2) instanceof Portlet);
+
+        Portlet portlet = (Portlet)html.getBody().getPOrH1OrH2().get(2);
+        assertEquals("webmill:search", portlet.getValue());
+        assertEquals("SearchXml", portlet.getXmlRoot());
+        assertEquals("SEARCH_CODE", portlet.getCode());
+
+        List<Object> elements = TemplateUtils.getElements(html);
+        assertEquals(3, elements.size());
+
+        assertTrue(elements.get(0) instanceof Custom);
+        assertTrue(elements.get(1) instanceof Dynamic);
+        assertTrue(elements.get(2) instanceof Portlet);
+    }
+}
