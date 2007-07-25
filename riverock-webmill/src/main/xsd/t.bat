bcf0c8b73d947ef0a4b53264c366189b56ffe3fd
Serge Maslyukov <19933474+sergmain@users.noreply.github.com>
2007-07-25 15:55:45 +0000

    Improved WSC Schema for template

--- riverock-webmill/src/main/xsd/t.bat
+++ riverock-webmill/src/main/xsd/t.bat
@@ -1,1 +1,6 @@
-C:\opt2\jaxb-ri-20070711\bin\xjc.bat riverock-template.xsd  -verbose  -b riverock-template.xjb -d ../java
+C:\opt2\jaxb-ri-20070122\bin\xjc.bat riverock-template.xsd  -verbose -p org.riverock.webmill.template.schema -b riverock-template.xjb -d ../java
+
+@rem C:\opt2\jaxb-ri-20070711\bin\xjc.bat riverock-template.xsd  -verbose -p org.riverock.webmill.template.schema -b riverock-template.xjb -d ../java
+
+@rem C:\opt2\jaxb-ri-20070711\bin\xjc.bat riverock-template.xsd  -verbose  -b riverock-template.xjb -d ../java
+@rem C:\opt2\jaxb-ri-20070122\bin\xjc.bat riverock-template.xsd  -verbose -b riverock-template.xjb -d ../java
