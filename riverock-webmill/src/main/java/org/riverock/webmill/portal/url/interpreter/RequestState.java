336adabc9d9fb6d75cf65454923a4c5bbb87c74f
Serge Maslyukov <19933474+sergmain@users.noreply.github.com>
2007-08-28 10:15:44 +0000

    Big refactoring

--- riverock-webmill/src/main/java/org/riverock/webmill/portal/url/interpreter/RequestState.java
+++ riverock-webmill/src/main/java/org/riverock/webmill/portal/url/interpreter/RequestState.java
@@ -22,7 +22,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
-package org.riverock.webmill.portal.url;
+package org.riverock.webmill.portal.url.interpreter;
 
 import javax.portlet.PortletMode;
 import javax.portlet.WindowState;
