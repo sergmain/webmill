eb3e0a9c2d0eb23fddb744986d2821170de9f984
serg_main <serg_main@c99c7d4a-b4a9-48f8-bb7e-b54423a2f45b>
2014-02-01 18:23:31 +0000

    .

---
+++ trunk/riverock-gwt/src/org/riverock/gwt/client/commons/CommonConst.java
@@ -0,0 +1,22 @@
+package org.riverock.gwt.client.commons;
+
+/**
+ * User: Serg
+ * Date: 01.02.14
+ * Time: 12:05
+ */
+public class CommonConst {
+    public static final String VALUE_1 = "1";
+    public static final String VALUE_0 = "0";
+    public static final String PERCENT_35 = "35%";
+    public static final String PERCENT_65 = "65%";
+    public static final String PERCENT_100 = "100%";
+
+    public static final String STYLE_ERROR_INPUT = "errorInput";
+    public static final String STYLE_AS_BUTTON = "asButton";
+    public static final String STYLE_GWT_VERTICAL_PANEL = "gwt-VerticalPanel";
+    public static final String STYLE_DEFAULT_TABLE = "defaultTable";
+
+    public static final String PIXEL_400 = "400px";
+    public static final String PIXEL_300 = "300px";
+}
